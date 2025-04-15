94b4246d10f0e4c8be59fbb850df478f789256c8
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2025-04-15 11:38:14 +0200

    ft: More features

--- src/main/java/org/example/ReadWriter.java
+++ src/main/java/org/example/ReadWriter.java
@@ -1,36 +1,62 @@
 package org.example;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class ReadWriter {
     public static void main(String[] args) {
-        readFromFile("files/example.txt");
-        writeToFile("files/file2.txt", "Lorem ipsum");
+        System.out.println("** WELCOME TO FILE READER AND WRITER PROGRAM **");
+        System.out.println("\n1. Write\n2. Read");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose an activity:");
+        int choice = sc.nextInt();
+        if(choice == 1) {
+            System.out.println("Provide the path of the file you want to read.");
+            String path = sc.next();
+            System.out.println("Now provide the text: ");
+            String text = sc.next();
+            writeToFile(path, text);
+        } else {
+
+        }
+//        writeToFile("files/example.txt", "Lorem ipsum");
+//        readFromFile("files/example.txt");
     }
 
     public static void readFromFile(String filePath) {
         try {
             File file = new File(filePath);
-            FileReader fr = new FileReader(file);
-            BufferedReader bfr = new BufferedReader(fr);
-            String line;
-            while ((line = bfr.readLine()) != null) {
-                System.out.println(line);
+            if (!file.exists()) {
+                System.out.println("The file you are trying to read is not available!");
+            } else {
+                FileReader fr = new FileReader(file);
+                BufferedReader bfr = new BufferedReader(fr);
+                String line;
+                while ((line = bfr.readLine()) != null) {
+                    System.out.println(line);
+                }
+                bfr.close();
             }
-            bfr.close();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 
     public static void writeToFile(String filePath, String text) {
         try {
             File file = new File(filePath);
-            FileWriter fw = new FileWriter(file);
-            BufferedWriter bwr = new BufferedWriter(fw);
-            bwr.write(text);
-            bwr.newLine();
-            bwr.close();
+//            if (!file.exists()) {
+//                file.createNewFile();
+//            }
+            if (text.length() == 0) {
+                System.out.println("No text provided");
+            } else {
+                FileWriter fw = new FileWriter(file, true);
+                BufferedWriter bwr = new BufferedWriter(fw);
+                bwr.write(text);
+                bwr.newLine();
+                bwr.close();
+            }
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
